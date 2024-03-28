878bc0904c5c507bb52c5bd796b46b856df12e15
skranz-auto <xsx50@uni-ulm.de>
2024-03-28 08:48:55 +0000

    restart

---
+++ scripts/install.R
@@ -0,0 +1,6 @@
+# Here we can add some custom package installations
+
+library(remotes)
+#remotes::install_github("repboxr/sourcemodify", upgrade="never", force=TRUE)
+#remotes::install_github("repboxr/repboxDB", upgrade="never", force=TRUE)
+remotes::install_github("repboxr/repboxReg", upgrade="never", force=TRUE)
