f3e0c04b024d5a3056105f104e50415a4a57ceec
skranz-auto <xsx50@uni-ulm.de>
2024-06-06 04:02:59 +0000

    restart

---
+++ README.md
@@ -0,0 +1,3 @@
+# Repbox via Github Actions
+
+A 3rd example with encrypted actual article and supplement. The encryption code is secret. This is only used as an internal test.
---
+++ scripts/install.R
@@ -0,0 +1,9 @@
+# Here we can add some custom package installations
+
+library(remotes)
+#remotes::install_github("repboxr/sourcemodify", upgrade="never", force=TRUE)
+#remotes::install_github("repboxr/repboxDB", upgrade="never", force=TRUE)
+#remotes::install_github("repboxr/repboxUtils", upgrade="never", force=TRUE)
+remotes::install_github("repboxr/repboxReg", upgrade="never", force=TRUE)
+remotes::install_github("repboxr/repboxStata", upgrade="never", force=TRUE)
+remotes::install_github("repboxr/repboxArt", upgrade="never", force=TRUE)
